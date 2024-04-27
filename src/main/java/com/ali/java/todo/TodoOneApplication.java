cf446e398fd13e1509c2a4e091d2c754a9877d7d
AliRabeeMahmoud <eng.alirabee@gmail.com>
2024-04-27 08:35:30 +0200

    start

---
+++ src/main/java/com/ali/java/todo/TodoOneApplication.java
@@ -0,0 +1,13 @@
+package com.ali.java.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ali/java/todo/TodoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.java.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
